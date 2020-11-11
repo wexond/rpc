735de493820477b00fe4b95e4af498d535652921
xnerhu <xnerhu@gmail.com>
2020-11-11 20:04:22 +0100

    feat: implement destroy in node worker receiver

--- packages/rpc-node/src/worker/worker-receiver.ts
+++ packages/rpc-node/src/worker/worker-receiver.ts
@@ -1,5 +1,5 @@
 import { MessagePort, Worker } from 'worker_threads';
-import { Receiver, RpcScaffold } from '@wexond/rpc-core';
+import { clearEvents, Receiver, RpcScaffold } from '@wexond/rpc-core';
 
 import {
   RpcWorkerHandler,
@@ -12,21 +12,27 @@ export class WorkerReceiver<T extends RpcScaffold<T>> extends Receiver<
   RpcWorkerHandler<T>,
   RpcWorkerObserver<T>
 > {
-  constructor(name: string, port: MessagePort | Worker) {
+  constructor(name: string, private readonly port: MessagePort | Worker) {
     super(name);
 
-    port.on('message', async (e: RpcWorkerRequest) => {
-      const caller = this.createCaller(e.method, e, ...e.args);
+    this.port.on('message', this.onMessage);
+  }
 
-      const { res, error } = await this.invokeRemoteHandler(caller);
+  private onMessage = async (e: RpcWorkerRequest) => {
+    const caller = this.createCaller(e.method, e, ...e.args);
 
-      port.postMessage({
-        id: e.id,
-        returnValue: res,
-        error,
-      } as RpcWorkerResponse);
+    const { res, error } = await this.invokeRemoteHandler(caller);
 
-      this.observers.notify(caller);
-    });
+    this.port.postMessage({
+      id: e.id,
+      returnValue: res,
+      error,
+    } as RpcWorkerResponse);
+
+    this.observers.notify(caller);
+  };
+
+  public destroy() {
+    this.port.removeListener('message', this.onMessage);
   }
 }
